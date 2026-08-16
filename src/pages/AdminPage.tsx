d77657a2085c4916b25993e970aa989c8f0fec94
Kaveena <kaveenaphy@gmail.com>
2026-08-16 04:38:28 +0300

    Fix CSV UTF-8 encoding

--- src/pages/AdminPage.tsx
+++ src/pages/AdminPage.tsx
@@ -169,10 +169,9 @@ function exportCSV() {
     )
     .join("\n");
 
-  const blob = new Blob([csv], {
-    type: "text/csv;charset=utf-8;",
-  });
-
+ const blob = new Blob(["\uFEFF" + csv], {
+  type: "text/csv;charset=utf-8;",
+});
   const url = URL.createObjectURL(blob);
 
   const link = document.createElement("a");
